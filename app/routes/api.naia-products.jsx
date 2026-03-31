0902c332609cc742c8e026177e8301a2d834093c
Nadeenabuobead <nadeenabuobead@gmail.com>
2026-03-31 11:52:43 +0300

    add naia products route

---
+++ app/routes/api.naia-products.jsx
@@ -0,0 +1,23 @@
+export async function loader() {
+  const response = await fetch(
+    "https://naia-test-store.myshopify.com/products.json?limit=20"
+  );
+  const data = await response.json();
+
+  const products = (data.products || []).map((p) => ({
+    id: p.id,
+    title: p.title,
+    handle: p.handle,
+    image: p.images?.[0]?.src || "",
+    url: `https://naia-9417.myshopify.com/products/${p.handle}`,
+  }));
+
+  return Response.json(
+    { products },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
+}
