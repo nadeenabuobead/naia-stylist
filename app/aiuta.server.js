e5051b607b6d55adbf78a3cf8419b66a83f3455b
nadeenabuobead <nadeenabuobead@gmail.com>
2026-04-05 19:24:04 +0300

    Delete app/aiuta.server.js

--- app/aiuta.server.js
+++
@@ -1,9 +0,0 @@
-export const AIUTA_BASE_URL =
-  process.env.AIUTA_BASE_URL || "https://api.aiuta.com/digital-try-on/v1";
-
-export function aiutaHeaders(extra = {}) {
-  return {
-    Authorization: `Bearer ${process.env.AIUTA_API_TOKEN}`,
-    ...extra,
-  };
-}
