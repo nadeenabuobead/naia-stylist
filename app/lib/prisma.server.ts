e33443c6acb60285dd9d5d63e80e1accaecba480
nadeenabuobead <nadeenabuobead@gmail.com>
2026-04-14 18:20:22 +0300

    Create prisma.server.ts

---
+++ app/lib/prisma.server.ts
@@ -0,0 +1,20 @@
+// app/lib/prisma.server.ts
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
+
+// Prevent multiple instances during development hot reloads
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.__prisma) {
+    global.__prisma = new PrismaClient();
+  }
+  prisma = global.__prisma;
+}
+
+export { prisma };
